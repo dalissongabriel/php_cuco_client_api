ec4d42d323660766d04d9075f28e06430c2c5d43
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-17 20:45:14 -0300

    test: must ensure that the client has valid email

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -4,7 +4,24 @@
 namespace App\Tests\Unit;
 
 
-class ClientTest
-{
+use App\Helpers\Exception\InvalidEmailException;
+use PHPUnit\Framework\TestCase;
+use App\Entity\Client;
 
+class ClientTest extends TestCase
+{
+    public function testShouldEnsureThatTheClientWillHaveAValidEmail()
+    {
+        $client = new Client();
+        $client->setEmail("test@valid.com");
+        self::assertEquals("test@valid.com", $client->getEmail());
+    }
+
+    public function testShouldEnsureValidationOfInvalidEmails()
+    {
+        self::expectException(InvalidEmailException::class);
+        $client = new Client();
+        $client->setEmail("test@");
+
+    }
 }
