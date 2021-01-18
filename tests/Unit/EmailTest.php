b6f33fdca0aea061dbeea125cfeabbd71bdc7234
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-18 00:45:15 -0300

    test: must ensure that the email value object is serializable

--- tests/Unit/EmailTest.php
+++ tests/Unit/EmailTest.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace App\Tests\ValueObject;
+namespace App\Tests\Unit;
 
+use App\Entity\Client;
 use App\ValueObject\Email;
+use JsonSerializable;
 use PHPUnit\Framework\TestCase;
 
 class EmailTest extends TestCase
 {
+    public function testShouldEnsureThatEntityIsSerializable()
+    {
+        $client = new Client();
+        $client->setEmail("teste@email.com");
 
+        self::assertTrue($client->getEmail() instanceof JsonSerializable);
+    }
 }
