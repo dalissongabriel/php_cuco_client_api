6b6c9de101fb6aa2ac7a2e775fc0d9218e5446c2
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-18 00:42:05 -0300

    test: must ensure that the cpf value object is serializable

---
+++ tests/Unit/CpfTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\Client;
+use App\ValueObject\Cpf;
+use JsonSerializable;
+use PHPUnit\Framework\TestCase;
+
+class CpfTest extends TestCase
+{
+    public function testShouldEnsureThatEntityIsSerializable()
+    {
+        $client = new Client();
+        $client->setCpf("278.128.110-77");
+
+        self::assertTrue($client->getCpf() instanceof JsonSerializable);
+    }
+}
