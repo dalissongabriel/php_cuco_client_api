4a52c271d5cfaecc68ded67d98ac90c2557939a6
dalissongabriel <alissongabrieldino@gmail.com>
2021-01-19 21:29:33 -0300

    test: must ensure login action work correctly

---
+++ tests/Integration/Web/LoginTest.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Tests\Integration\Web;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LoginTest extends WebTestCase
+{
+    public function testMustEnsureThatTheLoginRouteAcceptsOnlyRequestsWithMandatoryFields()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/login',[],[],[
+            'CONTENT_TYPE'=>'application/json'
+        ], json_encode([
+            "login"=>"test",
+            "senha"=>"password_test"
+        ]));
+
+        $response = $client->getResponse();
+        $content = json_decode($response->getContent());
+
+        self::assertFalse($content->success);
+        self::assertEquals(400, $response->getStatusCode());
+    }
+
+    public function testMustEnsureThatATokenIsReturnedWhenLoggingInCorrectly()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/login',[],[],[
+            'CONTENT_TYPE'=>'application/json'
+        ], json_encode([
+            "username"=>"user_cuco_api",
+            "password"=>'password_cuco_api'
+        ]));
+
+        $response = $client->getResponse();
+        $content = json_decode($response->getContent());
+        self::assertTrue($content->success);
+        self::assertObjectHasAttribute("access_token",$content->data);
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testShouldEnsureThatLoginAttemptsWithInvalidCredentialsAreNotAuthorized()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/login',[],[],[
+            'CONTENT_TYPE'=>'application/json'
+        ], json_encode([
+            "username"=>"my_aleatory_user",
+            "password"=>'my_aleatory_password'
+        ]));
+
+        $response = $client->getResponse();
+        $content = json_decode($response->getContent());
+
+        self::assertFalse($content->success);
+        self::assertObjectHasAttribute("message",$content->data);
+        self::assertEquals(401, $response->getStatusCode());
+    }
+
+}
